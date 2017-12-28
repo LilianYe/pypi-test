f7b6d1b46b76470646969809bad1822526f8b14d
叶巧敏 <lilian.ye@tianrang-inc.com>
2017-12-28 11:00:11 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='tr_db_utils',    # This is the name of your PyPI-package.
+    packages=['tr_db_utils'],  # This must be the same as the name above
+    version='0.1',                          # Update the version number for new releases
+    description='database utils',
+    author='lillianye',
+    author_email='qilixiangdeye@126.com',
+    url='https://github.com/LilianYe/pypi-test',
+    download_url='https://github.com/LilianYe/pypi-test/archive/0.1.tar.gz',
+    keywords=['database', 'api'],
+    classifiers=[],
+)
