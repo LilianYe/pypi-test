167669bbf17fcc9d8bcfa4c65a533824519757cb
叶巧敏 <lilian.ye@tianrang-inc.com>
2017-12-28 11:56:43 +0800

    modify version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='trdbhelp',    # This is the name of your PyPI-package.
     packages=['trdbhelp'],  # This must be the same as the name above
-    version='0.2',                          # Update the version number for new releases
+    version='0.1',                          # Update the version number for new releases
     description='database utils',
     author='lillianye',
     author_email='qilixiangdeye@126.com',
     url='https://github.com/LilianYe/pypi-test',
-    download_url='https://github.com/LilianYe/pypi-test/archive/0.2.tar.gz',
+    download_url='https://github.com/LilianYe/pypi-test/archive/0.1.tar.gz',
     keywords=['database', 'api'],
     classifiers=[],
 )
